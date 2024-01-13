21c626bfc022d4e4c5e5910212991b1eb411e25b
Emmanuel Iraturinze <Emmy-Iraturinzee>
2024-01-13 23:50:37 +0200

    new

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@
     // Sticky Navbar
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('bg-dark shadow-sm').css('top', '0px','ba');
+            $('.sticky-top').addClass('bg-dark shadow-sm').css('top', '0px');
         } else {
             $('.sticky-top').removeClass('bg-dark shadow-sm').css('top', '-150px');
         }
