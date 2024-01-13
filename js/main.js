4ff4953d1bc4ef95b955df7e9b92e816834a8e70
Emmanuel Iraturinze <Emmy-Iraturinzee>
2024-01-13 22:11:47 +0200

    new

--- js/main.js
+++ js/main.js
@@ -63,3 +63,16 @@
     
 })(jQuery);
 
+
+// scroll progress
+
+const scrollProgress = document.getElementById('scroll-progress');
+const height =
+document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener('scroll', () => {
+const scrollTop =
+document.body.scrollTop || document.documentElement.scrollTop;
+scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
+});
+
